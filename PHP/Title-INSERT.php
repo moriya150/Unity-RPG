c463e2c6a2e86bf9efb61e371d29d5b891c1e004
moriya takeru <mbttynbytn@yahoo.co.jp>
2022-12-08 15:12:13 +0900

    12/08 PHP修正

--- PHP/Title-INSERT.php
+++ PHP/Title-INSERT.php
@@ -1,17 +1,13 @@
 <?php
 
-require_once('rpg_connect.php');
-$db = connectDB();
+    require_once('rpg_connect.php');
+    $db = connectDB();
 
-$NAME = $_POST["NAME"];
+    $NAME = $_POST["NAME"];
 
-
-   //$sql = 'SELECT EXISTS (SELECT * FROM playertable WHERE NAME = "'.$NAME.'" LIMIT 1)';
-   $sql = 'INSERT INTO playertable (NAME,LV,KISO_HP,KISO_AT,KISO_DEF ,EXP,SOUBI_HP,SOUBI_AT,SOUBI_DEF) VALUES ("'.$NAME.'",1,100,25,5,0,0,0,0)';
+    $sql = 'INSERT INTO playertable (NAME,LV,KISO_HP,KISO_AT,KISO_DEF ,EXP,SOUBI_HP,SOUBI_AT,SOUBI_DEF) VALUES ("'.$NAME.'",1,100,25,5,0,0,0,0)';
 
     $result = mysqli_query($db, $sql);
-
-
 
     $db = mysqli_close($db);
 
@@ -19,6 +15,4 @@ $NAME = $_POST["NAME"];
     {
         exit('データベースとの接続を閉じられませんでした。');
     }
-
-    //echo $res;
 ?>
